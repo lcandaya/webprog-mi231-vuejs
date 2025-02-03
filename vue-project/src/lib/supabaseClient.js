689491deeb6b7ea6266502732f62764bbdce2d74
Lalainne Andaya <lcandaya2@student.apc.edu.ph>
2025-02-03 09:41:51 +0000

    supabaseClient.js

---
+++ vue-project/src/lib/supabaseClient.js
@@ -0,0 +1,3 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const supabase = createClient('https://yrmgnergqduislyfynvx.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlybWduZXJncWR1aXNseWZ5bnZ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc5NjgyOTMsImV4cCI6MjA1MzU0NDI5M30.Yw0bTcDeRBb7p8CxjMrtwOq8Iia3COnJ7UCHkyF9V4I')
